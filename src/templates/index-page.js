2ce921beacf4b2cd34af74a8589f7775d5b390dd
Oliver Heinz <o.heinz@linux.com>
2023-04-02 12:17:01 +0200

    modify

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -5,7 +5,7 @@ import { getImage } from "gatsby-plugin-image";
 
 import Layout from "../components/Layout";
 import Features from "../components/Features";
-import BlogRoll from "../components/BlogRoll";
+import DogRoll from "../components/DogRoll";
 import FullWidthImage from "../components/FullWidthImage";
 
 // eslint-disable-next-line
@@ -49,9 +49,9 @@ export const IndexPageTemplate = ({
                     <h3 className="has-text-weight-semibold is-size-2">
                       Câinii noștri
                     </h3>
-                    <BlogRoll />
+                    <DogRoll />
                     <div className="column is-12 has-text-centered">
-                      <Link className="btn" to="/blog">
+                      <Link className="btn" to="/dog">
                         Citește mai mult
                       </Link>
                     </div>
